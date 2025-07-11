0755ab68521004a1c0fce3a8649a6a99e4f9fd10
Emanuele <catalanoemanuele@outlook.com>
2025-07-11 16:17:45 +0200

    removed unfound packages for pacman repo, like fim, fixed driver.sh behavior by moving in another folder, that not overllap with function implementation inside lib file, tested twice once completed to be sure it works, as it is in this commit

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
+
+source ./driver.sh
 source ./lib
 
 for file in `find ./packages -type f`; do
 	installpackagesbatch $file
 done
+
+echo 'Precompiling formats for LaTeX'
+sudo fmtutil-sys -all
+echo 'Setup Complete.'
 
