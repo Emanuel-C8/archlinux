6960979aac307877f298cd87f7c174f4f7655b6d
Emanuele Catalano <catalanoemanuele@outlook.com>
2025-07-15 11:32:52 +0200

    Added X and DWM as a script, everything needed for it is automatically installed, just log out and log in again and will work, AMEND EDIT: just removed a ?fi? AMEND EDIT 2: Added install.sh file that executes X.sh and main.sh separately, in order to not create a conflict with sudo command. Added insertion of configuration files

---
+++ install.sh
@@ -0,0 +1,5 @@
+chmod +x X.sh main.sh
+
+./X.sh
+
+sudo ./main.sh
--- main.sh
+++ main.sh
@@ -1,4 +1,3 @@
-
 source ./driver.sh
 source ./lib
 
@@ -8,5 +7,10 @@ done
 
 echo 'Precompiling formats for LaTeX'
 sudo fmtutil-sys -all
+echo 'Copying file configurations in the local machine'
+sudo cp conf/dotfiles/.tmux.conf ~/
+sudo cp conf/unikeyboard conf/us /usr/share/X11/xkb/symbols
+sudo cp conf/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo cp conf/pacman.conf /etc
 echo 'Setup Complete.'
 
