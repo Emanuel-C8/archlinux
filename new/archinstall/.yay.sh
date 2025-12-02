f3923e91388caa3a924a72c58afc3d7c603c3351
Emanuele Catalano <catalanoemanuele@outlook.com>
2025-12-02 11:10:14 +0100

    added packages

---
+++ new/archinstall/.yay.sh
@@ -0,0 +1,12 @@
+sudo pacman -S go --noconfirm
+source ./lib
+git clone https://aur.archlinux.org/yay.git ~/yay
+cd ~/yay
+makepkg -si
+cd -
+
+for file in `find ./packages/YAY -type f`; do
+	yayinstallpackagesbatch $file
+done
+
+
